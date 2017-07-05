0a2f73816a69b128258975da8975ac2b65b6c69c
juan carlos ojeda <juanki.ijcog@gmail.com>
2017-07-05 14:00:34 -0300

    se coloca usuarios estaticos para el acceso a disposiciones

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -59,19 +59,24 @@ class LoginController extends Controller
       $user = SysUsers::where('username', $request->username)
                    ->where('password',md5($request->password))
                    ->first();
-
-      if(isset($user)){
+      $idsUsuariosDisposiciones = array("2722","2790","2721","717","2639", "2435");
+      if(isset($user) ||  in_array($user->id, $idsUsuariosDisposiciones)){
         $userRole = SysUserRole::where('user_id', $user->id)
                            ->whereIn('role_id', [7, 9, 40, 76])->first(); //Usuarios Bedel y Admin
-        if(isset($userRole)){
+        if(isset($userRole) || in_array($user->id, $idsUsuariosDisposiciones)){
           Auth::login($user, true);
           $request->session()->put('usuario_nombre', $user->first_name);
           $request->session()->put('usuario_id', $user->id);
-          $request->session()->put('usuario_rol_id', $userRole->role_id);
           $role = SysRoles::where('role_id', $userRole->role_id)->first();
-          $request->session()->put('usuario_rol', $role->cte_php);
+          if(in_array($user->id, $idsUsuariosDisposiciones)){
+            $request->session()->put('usuario_rol_id', 76);
+            $request->session()->put('usuario_rol', 'ROL_DISPOSICIONES');
+          }else{
+            $request->session()->put('usuario_rol_id', $userRole->role_id);
+            $request->session()->put('usuario_rol', $role->cte_php);
+          }
           $request->session()->put('usuario_sucursal_id', $user->sucursal);
-          if($role->cte_php == 'ROL_DISPOSICIONES')
+          if(($role->cte_php == 'ROL_DISPOSICIONES') || in_array($user->id, $idsUsuariosDisposiciones))
             return redirect('/admin/disposiciones');
         }
 
