c263513e651e93854632609b4a2e37d82aa9d0cb
Juan Ojeda <jojeda.dghct@gmail.com>
2018-06-12 10:55:48 -0300

    se actualiza precheck y ws errores

--- app/Http/Controllers/MicroservicioController.php
+++ app/Http/Controllers/MicroservicioController.php
@@ -17,16 +17,16 @@ class MicroservicioController extends Controller
       $this->cargarEstados();
       $tramitesAIniciar = new TramitesAInicarController();
       //  pasa a estado 1
-      //$tramitesAIniciar->comletarTurnosEnTramitesAIniciar( $this->estados->INICIO );
+      $tramitesAIniciar->comletarTurnosEnTramitesAIniciar( $this->estados->INICIO );
       // pasa de estado 1 a 2 los tramites
-      //$tramitesAIniciar->completarBoletasEnTramitesAIniciar( $this->estados->INICIO, $this->estados->SAFIT);
+      $tramitesAIniciar->completarBoletasEnTramitesAIniciar( $this->estados->INICIO, $this->estados->SAFIT);
       // Emitir cenat solo si estado 2
-      //$tramitesAIniciar->emitirBoletasVirtualPago( $this->estados->SAFIT, $this->estados->EMISION_BOLETA_SAFIT,   $this->estados->VALIDACIONES); //ID validacion 3
+      $tramitesAIniciar->emitirBoletasVirtualPago( $this->estados->SAFIT, $this->estados->EMISION_BOLETA_SAFIT,   $this->estados->VALIDACIONES); //ID validacion 3
       // Emitir cenat solo si estado 1 ya actualiza validaciones_precheck
-      //$tramitesAIniciar->verificarLibreDeudaDeTramites($this->estados->INICIO, $this->estados->LIBRE_DEUDA, $this->estados->VALIDACIONES); //ID validacion 4
+      $tramitesAIniciar->verificarLibreDeudaDeTramites($this->estados->INICIO, $this->estados->LIBRE_DEUDA, $this->estados->VALIDACIONES); //ID validacion 4
       // Emitir cenat solo si estado 1 ya actualiza validaciones_precheck
-      //$tramitesAIniciar->verificarBuiTramites( $this->estados->INICIO, $this->estados->BUI, $this->estados->VALIDACIONES); //ID validacion 5
-      $tramitesAIniciar->revisarValidaciones($this->estados->VALIDACIONES_COMPLETAS, $this->estados->VALIDACIONES);
+      $tramitesAIniciar->verificarBuiTramites( $this->estados->INICIO, $this->estados->BUI, $this->estados->VALIDACIONES); //ID validacion 5
+      $tramitesAIniciar->revisarValidaciones($this->estados->VALIDACIONES_COMPLETAS);
       // Si bui, cenat y infracciones pasa de estado 2 a 6
       // pasa de estado 6 a 7 los tramites
       //$tramitesAIniciar->enviarTramitesASinalic( $this->estados->VALIDACIONES, $this->estados->INICIO_EN_SINALIC);
@@ -41,7 +41,7 @@ class MicroservicioController extends Controller
       $this->estados->EMISION_BOLETA_SAFIT = SysMultivalue::where('text_id', 'EMISION_BOLETA_SAFIT')->where('type', 'VALP')->first()->id;
       $this->estados->LIBRE_DEUDA = SysMultivalue::where('text_id', 'LIBRE_DEUDA')->where('type', 'VALP')->first()->id;
       $this->estados->BUI = SysMultivalue::where('text_id', 'BUI')->where('type', 'VALP')->first()->id;
-      $this->estados->VALIDACIONES_COMPLETAS = SysMultivalue::where('text_id', 'VALIDACIONES_COMPLETAS')->where('type', 'VALP')->first()->id;
+      $this->estados->VALIDACIONES_COMPLETAS = SysMultivalue::where('text_id', 'VALIDACIONES_COMPLETAS')->where('type', 'AUTO')->first()->id;
       $this->estados->INICIO_EN_SINALIC = SysMultivalue::where('text_id', 'INICIO_EN_SINALIC')->where('type', 'AUTO')->first()->id;
     }
 }
--- resources/views/safit/resultCheckModoAutonomo.blade.php
+++ resources/views/safit/resultCheckModoAutonomo.blade.php
@@ -1,7 +1,7 @@
 
 <div class="clearfix"></div>
 <div class="row">
-  <div class="col-md-6">
+  <div class="col-md-6 col-sm-6 col-xs-12">
     <div class="x_panel">
       <div class="x_title">
         <h2>Datos Tramite <small></small></h2>
@@ -29,27 +29,19 @@
             </li>
           </ul>
         </div>
-          <div class="clearfix"></div>
+        <div id="logTurno">
+          
+        </div>
+        <div class="clearfix"></div>
       </div>
     </div>
   </div>
-  <div class="col-md-6">
+  <div class="col-md-6 col-sm-6 col-xs-12">
     <div class="x_panel">
       <div class="x_title">
         <h2>Log de Pre-Check <small></small></h2>
         <ul class="nav navbar-right panel_toolbox">
           <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Settings 1</a>
-              </li>
-              <li><a href="#">Settings 2</a>
-              </li>
-            </ul>
-          </li>
-          <li><a class="close-link"><i class="fa fa-close"></i></a>
           </li>
         </ul>
         <div class="clearfix"></div>
