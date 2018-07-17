7d06e75de8dfabd3e38a607aee873f0f3eccd8f5
Juan Ojeda <jojeda.dghct@gmail.com>
2018-07-17 09:25:33 -0300

    creacion tabla relacion tipo docs

---
+++ database/migrations/2018_07_17_082306_create_tipo_doc_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipoDocTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_doc', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_dgevyl');
+            $table->integer('id_sigeci');
+            $table->timestamps();
+        });
+        DB::table('tipo_doc')->insert(array(
+            'id_dgevyl' => '1',
+            'id_sigeci' => '1',
+            'created_at' => date(),
+            'updated_at' => date()
+        ));
+        DB::table('tipo_doc')->insert(array(
+            'id_dgevyl' => '1',
+            'id_sigeci' => '2',
+            'created_at' => date(),
+            'updated_at' => date()
+        ));
+        DB::table('tipo_doc')->insert(array(
+            'id_dgevyl' => '1',
+            'id_sigeci' => '3',
+            'created_at' => date(),
+            'updated_at' => date()
+        ));
+        DB::table('tipo_doc')->insert(array(
+            'id_dgevyl' => '1',
+            'id_sigeci' => '4',
+            'created_at' => date(),
+            'updated_at' => date()
+        ));
+        DB::table('tipo_doc')->insert(array(
+            'id_dgevyl' => '1',
+            'id_sigeci' => '5',
+            'created_at' => date(),
+            'updated_at' => date()
+        ));
+        DB::table('tipo_doc')->insert(array(
+            'id_dgevyl' => '4',
+            'id_sigeci' => '6',
+            'created_at' => date(),
+            'updated_at' => date()
+        ));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_doc');
+    }
+}
