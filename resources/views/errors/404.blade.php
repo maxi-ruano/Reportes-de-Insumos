21e91fd650eaa0234a8861daa3bbb16130ebcba5
Daniela Romero <dromero.dghct@gmail.com>
2018-08-14 13:18:53 -0300

    modificacion en routes y views de los errores 404 y 500

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,13 @@
-@extends('layouts.templeate')
-@section('titlePage', 'Estimado usuario:')
+@extends('layouts.app')
 
 @section('content')
-    <h2 class='red'>La URL que está intentando ingresar NO ES VÁLIDA!</h2>
+    <div style="max-width:600px; margin:0px auto;">   
+        <h2><b>Estimado usuario:</b></h2>
+        <h3 style='color:red'>La URL que está intentando ingresar NO ES VÁLIDA!</h3>
 
-    <p> Error 404: Indica que la página que se está tratando de cargar no se ha encontrado. <br>
-        Esto puede ocurrir porque hemos escrito mal la dirección de la página web que queremos cargar, que hemos cargado la página a través de un enlace erróneo, o que la página sí que existió en su momento pero ahora ya no.</p>
+        <p> Error 404: Indica que la página que se está tratando de cargar no se ha encontrado. <br>
+            Esto puede ocurrir porque hemos escrito mal la dirección de la página web que queremos cargar, que hemos cargado la página a través de un enlace erróneo, o que la página sí que existió en su momento pero ahora ya no.</p>
 
-    Por favor, para ir a la página de inicio haga <a href="{{ url('login')}}" class="blue"> click aqui</a>
+        Por favor, para ir a la página de inicio haga <a href="{{ url('login')}}"> click aqui</a>
+    </div>
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,8 +1,12 @@
-@extends('layouts.templeate')
-@section('titlePage', 'Estimado usuario:')
+@extends('layouts.app')
 
 @section('content')
-    <h2><span class='red'> Se ha producido un error en el sistema! </span> </h2>
-    Por favor, comuniquese con el Administrador del sistema.
-    <p>Si deseas ir a la página de inicio haga <a href="{{ url('login')}}" class="blue"> click aqui</a> </p>
+    <div style="max-width:600px; margin:0px auto;">   
+        <h3><b>Estimado usuario:</b></h3>
+        <h3 style='color:red'>Se ha producido un error en el sistema!</h3>
+
+        <p> Error 500: Indica un error interno, lo que se puede traducir que la página web tiene algún error en el código, por lo que el servidor no puede generar el código HTML.</p>
+
+        Por favor, para ir a la página de inicio haga <a href="{{ url('login') }}"> click aqui</a>
+    </div>
 @endsection
