48a864db561e8bcd078ab36b52c3b75b17e6b8e7
Daniela Romero <dromero.dghct@gmail.com>
2018-09-11 15:04:23 -0300

    se agrega a sys_config la configuracion de produccion - verificar

--- database/migrations/2018_08_15_103150_add_sys_config_precheck.php
+++ database/migrations/2018_08_15_103150_add_sys_config_precheck.php
@@ -16,7 +16,7 @@ class AddSysConfigPrecheck extends Migration
         DB::table('sys_config')->insert(array(
             'name' => 'SafitWS',
             'param' => 'userPass',
-            'value' => '1sdfr45g347dkf8gs0d',
+            'value' => 'weporjgsdf41654',
             'description' => 'Password usuario conexion webservice SAFIT',
             'created_by' => '2828',
             'modified_by' => '2829',
@@ -27,7 +27,7 @@ class AddSysConfigPrecheck extends Migration
         DB::table('sys_config')->insert(array(
             'name' => 'SafitWS',
             'param' => 'userName',
-            'value' => '000004',
+            'value' => '000016',
             'description' => 'Usuario conexion webservice SAFIT',
             'created_by' => '2828',
             'modified_by' => '2829',
@@ -49,8 +49,8 @@ class AddSysConfigPrecheck extends Migration
         DB::table('sys_config')->insert(array(
             'name' => 'SafitWS',
             'param' => 'ws_url',
-            'value' => 'https://testing.safit.com.ar/service/s_001.php?wsdl',
-            'description' => 'Usuario Hash conexion webservice SAFIT',
+            'value' => 'https://www.safit.com.ar/service/s_001.php?wsdl',
+            'description' => 'URL conexion webservice SAFIT',
             'created_by' => '2828',
             'modified_by' => '2829',
             'creation_date' => null,
@@ -89,6 +89,100 @@ class AddSysConfigPrecheck extends Migration
             'creation_date' => null,
             'modification_date' => date("Y-m-d H:i:s")
         ));
+
+
+        //WS BUI 
+        DB::table('sys_config')->insert(array(
+            'name' => 'BuiWS',
+            'param' => 'userPass',
+            'value' => 'lic189',
+            'description' => 'Password usuario conexion webservice BUI',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+
+        DB::table('sys_config')->insert(array(
+            'name' => 'BuiWS',
+            'param' => 'userName',
+            'value' => 'licenciasws',
+            'description' => 'Usuario conexion webservice BUI',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+
+        DB::table('sys_config')->insert(array(
+            'name' => 'BuiWS',
+            'param' => 'ws_url',
+            'value' => 'http://10.73.100.42:6748/service/api/BUI/GetResumenBoletasPagas',
+            'description' => 'URL conexion webservice BUI',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+
+        DB::table('sys_config')->insert(array(
+            'name' => 'BuiWS',
+            'param' => 'enabled',
+            'value' => 'true',
+            'description' => 'Habilitar Deshabitar WS',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+        
+
+        /*//WS LibreDeuda - Ya se encuenta en BD
+        DB::table('sys_config')->insert(array(
+            'name' => 'LibreDeudaWS',
+            'param' => 'userPass',
+            'value' => 'LICWEB',
+            'description' => 'Password usuario conexion webservice LIBRE DEUDA',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+
+        DB::table('sys_config')->insert(array(
+            'name' => 'LibreDeudaWS',
+            'param' => 'userName',
+            'value' => 'LICENCIAS01',
+            'description' => 'Usuario conexion webservice LIBRE DEUDA',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+
+        DB::table('sys_config')->insert(array(
+            'name' => 'LibreDeudaWS',
+            'param' => 'wsdl_url',
+            'value' => 'https://tcaba2.dgai.com.ar/LicenciaWS/LicenciaWS?',
+            'description' => 'URL conexion webservice LIBRE DEUDA',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+
+        DB::table('sys_config')->insert(array(
+            'name' => 'LibreDeudaWS',
+            'param' => 'enabled',
+            'value' => 'true',
+            'description' => 'Habilitar Deshabitar WS',
+            'created_by' => '2828',
+            'modified_by' => '2829',
+            'creation_date' => null,
+            'modification_date' => date("Y-m-d H:i:s")
+        ));
+        */
+
     }
 
     /**
@@ -99,5 +193,7 @@ class AddSysConfigPrecheck extends Migration
     public function down()
     {
         DB::table('sys_config')->where('name', 'SafitWS')->delete();
+        DB::table('sys_config')->where('name', 'BuiWS')->delete();
+        //DB::table('sys_config')->where('name', 'LibreDeudaWS')->delete();
     }
 }
