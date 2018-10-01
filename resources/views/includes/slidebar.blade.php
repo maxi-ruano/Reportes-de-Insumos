2822645ad7890cbbc341af4e5e78f42f6b027da5
dromero <dromero.dghct@gmail.com>
2018-10-01 09:18:15 -0300

    se corrige error de auth() al acceder sin estar logeado

--- resources/views/includes/slidebar.blade.php
+++ resources/views/includes/slidebar.blade.php
@@ -1,37 +1,38 @@
-
 <!-- sidebar menu -->
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
   <div class="menu_section">
     <h3>Menú</h3>
     <ul class="nav side-menu">
-      
-      @if(auth()->user()->can('view_all_tramites_habilitados') || auth()->user()->can('view_self_tramites_habilitados')  || auth()->user()->can('view_sede_tramites_habilitados'))
-      <li><a href="{{ route('tramitesHabilitados.index') }}?fecha={{date('Y-m-d')}}">
-        <i class="fa fa-street-view"></i> Tramites Habilitados
-        <span class="fa fa-chevron-right"></span></a>
-      </li>
-      @else
-        @can('add_tramites_habilitados')
-        <li><a href="{{ route('tramitesHabilitados.create') }}">
-          <i class="fa fa-street-view"></i> Tramites Habilitados
+      @if(Auth::check())
+        @if(Auth::user()->can('view_all_tramites_habilitados') || Auth::user()->can('view_self_tramites_habilitados') || Auth::user()->can('view_sede_tramites_habilitados'))
+          <li><a href="{{ route('tramitesHabilitados.index') }}?fecha={{date('Y-m-d')}}">
+            <i class="fa fa-street-view"></i> Tramites Habilitados
+            <span class="fa fa-chevron-right"></span></a>
+          </li>
+        @else
+          @can('add_tramites_habilitados')
+            <li><a href="{{ route('tramitesHabilitados.create') }}">
+              <i class="fa fa-street-view"></i> Tramites Habilitados
+              <span class="fa fa-chevron-right"></span></a>
+            </li>
+          @endcan
+        @endif
+
+        @can('view_dashboard')
+        <li><a href="{{ route('consultaDashboard') }}">
+          <i class="fa fa-pie-chart"></i> Estadisticas
           <span class="fa fa-chevron-right"></span></a>
         </li>
         @endcan
+
+        @can('view_users')
+        <li><a href="{{ route('users.index') }}">
+            <i class="fa fa-users"></i> Usuarios
+            <span class="fa fa-chevron-right"></span></a>
+        </li>
+        @endcan
+
       @endif
-
-      @can('view_dashboard')
-      <li><a href="{{ route('consultaDashboard') }}">
-        <i class="fa fa-pie-chart"></i> Estadisticas
-        <span class="fa fa-chevron-right"></span></a>
-      </li>
-      @endcan
-
-      @can('view_users')
-      <li><a href="{{ route('users.index') }}">
-          <i class="fa fa-users"></i> Usuarios
-          <span class="fa fa-chevron-right"></span></a>
-      </li>
-      @endcan
 
       @if(session('usuario_rol_id') == '40' || session('usuario_rol') == 'ROL_ESCUELA' || session('usuario_rol') == 'ROL_ADMIN'
 	|| session('usuario_id') == '2722' || session('usuario_id') == '2790' || session('usuario_id') == '2432')
