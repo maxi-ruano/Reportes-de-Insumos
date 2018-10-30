602334f8bb3f7a2a26e48a50ffe27288bab93f30
Daniela Romero <dromero.dghct@gmail.com>
2018-10-30 12:38:08 -0300

    se aplica permisos para acceder a Generar Precheck segun la IP

---
+++ database/migrations/2018_10_30_101938_create_emision_boleta_safit_permisos_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmisionBoletaSafitPermisosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emision_boleta_safit_permisos', function (Blueprint $table) {
+            $table->integer('sucursal_id');
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+
+        //Habilitar para todas las sucursales menos para: Roca y La Nueva
+        DB::table('emision_boleta_safit_permisos')->insert([
+            ['sucursal_id' => '1', 'activo' => false, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '10', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '20', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '30', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '40', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '50', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '60', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '70', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '80', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '90', 'activo' => false, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '100', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '103', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '110', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '120', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '121', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '130', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ],
+            ['sucursal_id' => '140', 'activo' => true, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emision_boleta_safit_permisos');
+    }
+}
