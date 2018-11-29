87f403e63f3eb89f1949cf21eddfe154c6345406
root <root@localhost.localdomain>
2018-11-29 10:11:42 -0300

    reestablecer configuracion horaria del precheck

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,10 +31,12 @@ class Kernel extends ConsoleKernel
         $schedule->call('App\Http\Controllers\MicroservicioController@completarTurnosEnTramitesAIniciar')->weekdays()->at('15:30');//Turnos
         $schedule->call('App\Http\Controllers\MicroservicioController@verificarLibreDeudaDeTramites')->weekdays()->at('15:31');//Libre Deuda
         $schedule->call('App\Http\Controllers\MicroservicioController@completarBoletasEnTramitesAIniciar')->weekdays()->at('15:32');//Buscar Boleta Cenat
-        $schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('15:34');//Obtener Certificado Virtual Cenat
+        //$schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('15:34');//Obtener Certificado Virtual Cenat
         $schedule->call('App\Http\Controllers\MicroservicioController@verificarBuiTramites')->weekdays()->at('15:35');//Bui
-        $schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('15:36');//Turnos Vencidos
-        //$schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('17:00');//Turnos a Enviar a Sinalic
+
+	$schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('17:00');//Obtener Certificado Virtual Cenat
+	$schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('17:40');//Validaciones Completas
+        $schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('18:00');//Turnos a Enviar a Sinalic
         //FIN PRECHECK
     }
 
