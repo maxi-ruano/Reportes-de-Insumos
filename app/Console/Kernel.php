5cde935407e06c23943365790026bbfe400f464f
dromero <dromero.dghct@gmail.com>
2019-01-21 11:19:05 -0300

    se habilita el inicio en Sinalic desde el Precheck

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,18 +25,23 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //Turnos Vencidos Precheck
-        $schedule->call('App\Http\Controllers\MicroservicioController@revisarTurnosVencidos')->weekdays()->at('15:20');
+        $schedule->call('App\Http\Controllers\MicroservicioController@revisarTurnosVencidos')->weekdays()->at('15:00');
+
+        //Enviar a Sinalic los tramites con Precheck completados
+        $schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('15:05');//Validaciones Completas
+        $schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('15:10');//Turnos a Enviar a Sinalic
 
         //PRECHECK
         $schedule->call('App\Http\Controllers\MicroservicioController@completarTurnosEnTramitesAIniciar')->weekdays()->at('15:30');//Turnos
         $schedule->call('App\Http\Controllers\MicroservicioController@verificarLibreDeudaDeTramites')->weekdays()->at('15:31');//Libre Deuda
         $schedule->call('App\Http\Controllers\MicroservicioController@completarBoletasEnTramitesAIniciar')->weekdays()->at('15:32');//Buscar Boleta Cenat
-        //$schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('15:34');//Obtener Certificado Virtual Cenat
         $schedule->call('App\Http\Controllers\MicroservicioController@verificarBuiTramites')->weekdays()->at('15:35');//Bui
 
 	    $schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('17:00');//Obtener Certificado Virtual Cenat
 	    $schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('17:40');//Validaciones Completas
-        //$schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('18:00');//Turnos a Enviar a Sinalic
+
+        //Enviar a Sinalic el resto de los completados
+        $schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('18:00');//Turnos a Enviar a Sinalic
         //FIN PRECHECK
     }
 
