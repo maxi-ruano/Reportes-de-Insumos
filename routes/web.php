1a3454fe6289f55b63f909de23324a37757e485d
dromero <dromero.dghct@gmail.com>
2019-03-29 12:08:02 -0300

    se aplica auth a las routes web

--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,40 @@
 
 //LOGIN
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', function (){ return redirect('/login'); });
+
 Route::group( ['middleware' => ['auth']], function() {
+  Route::get('/home', 'HomeController@index')->name('home');
   Route::resource('users', 'UserController');
   Route::resource('roles', 'RoleController');
-  
   Route::get('/changePassword','Auth\ResetPasswordController@showChangePasswordForm');
   Route::post('/changePassword','Auth\ResetPasswordController@changePassword')->name('changePassword');
   Route::get('/activarCuentaUser', ['uses' => 'UserController@activar','as' => 'UserController.activar']);
+
+  //TRAMITES HABILITADOS
+  Route::resource('tramitesHabilitados','TramitesHabilitadosController');
+  Route::get('tramitesHabilitadosHabilitar', ['uses' => 'TramitesHabilitadosController@habilitar','as' => 'tramitesHabilitados.habilitar']);
+  Route::get('buscarDatosPersonales', 'TramitesHabilitadosController@buscarDatosPersonales');
+  Route::get('consultarUltimoTurno', 'TramitesHabilitadosController@consultarUltimoTurno');
+  Route::get('consultarTurnoSigeci', 'TramitesHabilitadosController@consultarTurnoSigeci');
+  Route::get('consultarTramite', 'TramitesController@consultarTramite');
+
+  //TRAMITES HABILITADOS MOTIVOS
+  Route::resource('tramitesHabilitadosMotivos','TramitesHabilitadosMotivosController');
+  Route::get('tramitesHabilitadosMotivosHabilitar', ['uses' => 'TramitesHabilitadosMotivosController@habilitar','as' => 'tramitesHabilitadosMotivos.habilitar']);
+  Route::resource('roleMotivos','RoleMotivosController');
+
+  //MODIFICACIONES DEL SISTEMA
+  Route::resource('precheck', 'PreCheckController');
+  Route::get('anularPreCheck','PreCheckController@anular')->name('anular');
+  Route::get('listado_teoicopc','TeoricoPcController@getTeoricoPc')->name('getTeoricoPc');
+  Route::get('cambiarPcTeorico','TeoricoPcController@cambiarPcTeorico')->name('cambiarPcTeorico');
+  Route::get('desactivarPcTeorico','TeoricoPcController@desactivarPcTeorico')->name('desactivarPcTeorico');
+  Route::resource('examen', 'EtlExamenController');
+  Route::get('listado_examenes','EtlExamenController@getExamenes')->name('getExamenes');
+  Route::get('anular_examen','EtlExamenController@anular')->name('anular');
+
 });
-
-//TRAMITES HABILITADOS
-Route::resource('tramitesHabilitados','TramitesHabilitadosController');
-Route::get('tramitesHabilitadosHabilitar', ['uses' => 'TramitesHabilitadosController@habilitar','as' => 'tramitesHabilitados.habilitar']);
-Route::get('buscarDatosPersonales', 'TramitesHabilitadosController@buscarDatosPersonales');
-Route::get('consultarUltimoTurno', 'TramitesHabilitadosController@consultarUltimoTurno');
-Route::get('consultarTurnoSigeci', 'TramitesHabilitadosController@consultarTurnoSigeci');
-Route::get('consultarTramite', 'TramitesController@consultarTramite');
-//end TRAMITES HABILITADOS
-
-//TRAMITES HABILITADOS MOTIVOS
-Route::resource('tramitesHabilitadosMotivos','TramitesHabilitadosMotivosController');
-Route::get('tramitesHabilitadosMotivosHabilitar', ['uses' => 'TramitesHabilitadosMotivosController@habilitar','as' => 'tramitesHabilitadosMotivos.habilitar']);
-Route::resource('roleMotivos','RoleMotivosController');
-//end TRAMITES HABILITADOS MOTIVOS
 
 //DASHBOARD
 Route::get('consultaDashboard', ['uses' => 'DashboardController@consultaDashboard','as' => 'consultaDashboard']);
@@ -51,22 +60,7 @@ Route::get('consultaTurnosEnEsperaPorSucursal', ['uses' => 'DashboardController@
 Route::get('run', 'MicroservicioController@run');
 Route::get('runPrecheck','MicroservicioController@runPrecheck')->name('runPrecheck');
 Route::get('generarCenatPrecheck','TramitesAInicarController@generarCenatPrecheck')->name('generarCenatPrecheck');
-
-Route::resource('precheck', 'PreCheckController');
-Route::get('anularPreCheck','PreCheckController@anular')->name('anular');
 //end PRECHECK
-
-Route::get('listado_examenes','EtlExamenController@getExamenes')->name('getExamenes');
-Route::get('anular_examen','EtlExamenController@anular')->name('anular');
-
-Route::post('rendir_examen',['uses' => 'EtlExamenPreguntaController@getPreguntasExamen','as' => 'rendir_examen']);
-Route::get('guardar_respuesta',['uses' => 'EtlExamenPreguntaController@guardarRespuesta','as' => 'guardaRespuesta']);
-Route::post('finalizar_examen',['uses' => 'EtlExamenController@calcularYGuardarResultado','as' => 'finalizar_examen']);
-// Route::get('/address/{id}/destroy',['uses' => 'AddressesController@destroy','as' => 'sysfile.addresses.destroy']);
-
-Route::get('listado_teoicopc','TeoricoPcController@getTeoricoPc')->name('getTeoricoPc');
-Route::get('cambiarPcTeorico','TeoricoPcController@cambiarPcTeorico')->name('cambiarPcTeorico');
-Route::get('desactivarPcTeorico','TeoricoPcController@desactivarPcTeorico')->name('desactivarPcTeorico');
 
 //SAFIT
 Route::get('buscarBoletaPago', 'TramitesAInicarController@buscarBoletaPago');
@@ -76,39 +70,9 @@ Route::get('checkPreCheck', ['uses' => 'PreCheckController@checkPreCheck','as' =
 Route::get('consultarPreCheck', ['uses' => 'PreCheckController@consultarPreCheck','as' => 'consultarPreCheck']);
 Route::get('buscarTramitesPrecheck', ['uses' => 'PreCheckController@buscarTramitesPrecheck','as' => 'buscarTramitesPrecheck']);
 Route::get('testCheckBoletas', ['uses' => 'TramitesAInicarController@testCheckBoletas','as' => 'testCheckBoletas']);
-
 Route::get('buscarBoletaPagoPersona', 'TramitesAInicarController@buscarBoletaPagoPersona');
 Route::post('buscarBoletaPagoPersona',['uses' => 'TramitesAInicarController@buscarBoletaPagoPersona','as' => 'buscarBoletaPagoPersona']);
 //END SAFIT
-
-//Auth::routes();
-Route::get('computadorasMonitor', 'TeoricoPcController@computadorasMonitor');
-Route::get('verificarAsignacion', 'TeoricoPcController@verificarAsignacion');
-//Route::group(['middleware' => 'web' ], function () {
-//Auth::routes();
-Route::auth();
-  Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
-      Route::get('computadoras/active', 'TeoricoPcController@isActive');
-      Route::resource('computadoras','TeoricoPcController');
-      Route::resource('bedel', 'BedelController');
-      Route::resource('disposiciones', 'DisposicionesController');
-      Route::get('asignar','BedelController@asignarExamen')->name('asignarExamen');
-      Route::post('buscarTramite',['uses' => 'TramitesController@buscarTramite','as' => 'buscarTramite']);
-
-      Route::get('reporteSecuenciaInsumos',['uses' => 'ReportesController@reporteSecuenciaInsumos','as' => 'reporteSecuenciaInsumos']);
-      Route::get('reporteControlInsumos',['uses' => 'ReportesController@reporteControlInsumos','as' => 'reporteControlInsumos']);
-      Route::get('justificar',['uses' => 'ReportesController@justificar','as' => 'justificar']);
-      Route::post('justificaciones.store',['uses' => 'ReportesController@justificacionStore','as' => 'justificaciones.store']);
-      Route::get('justificaciones.edit/{id}',['uses' => 'ReportesController@justificar','as' => 'justificaciones.edit']);
-      Route::get('justificaciones.show/{id}',['uses' => 'ReportesController@mostrarJustificacion','as' => 'justificaciones.show']);
-
-  });
-//});
-// Route::get('rendir_examen','EtlExamenController@rendir_examen');
-Route::resource('examen', 'EtlExamenController');
-Route::resource('preguntas', 'EtlExamenPreguntaController');
-
-///var/www/html/teorico/app/Http/Controllers/EtlExamenController.php
 
 Route::group(['prefix' => 'api'], function () {
   Route::get('aviso_pago', 'SoapServerController@index')->name('aviso_pago');
