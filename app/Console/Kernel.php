8625fbbc61fb7497d7682ca8faa4507e88f25e3e
dromero <dromero.dghct@gmail.com>
2020-01-14 14:40:43 -0300

    se corrige horario de precheck

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,21 +24,19 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //Turnos Vencidos Precheck
-        $schedule->call('App\Http\Controllers\MicroservicioController@revisarTurnosVencidos')->weekdays()->at('16:00');
+        //Turnos vencidos
+        $schedule->call('App\Http\Controllers\MicroservicioController@revisarTurnosVencidos')->weekdays()->at('17:00');
 
         //PRECHECK
-        $schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('16:05');//Validaciones Completas
-        $schedule->call('App\Http\Controllers\MicroservicioController@completarTurnosEnTramitesAIniciar')->weekdays()->at('16:10');//Turnos
-        $schedule->call('App\Http\Controllers\MicroservicioController@verificarLibreDeudaDeTramites')->weekdays()->at('16:12');//Libre Deuda
-        $schedule->call('App\Http\Controllers\MicroservicioController@completarBoletasEnTramitesAIniciar')->weekdays()->at('16:14');//Buscar Boleta Cenat
-        $schedule->call('App\Http\Controllers\MicroservicioController@verificarBuiTramites')->weekdays()->at('16:16');//Bui
-
-	    $schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('17:00');//Obtener Certificado Virtual Cenat
-	    $schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('17:50');//Validaciones Completas
-
-        //Enviar a Sinalic el resto de los completados
-        $schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('18:00');//Turnos a Enviar a Sinalic
+        $schedule->call('App\Http\Controllers\MicroservicioController@completarTurnosEnTramitesAIniciar')->weekdays()->at('17:10');//Turnos
+        $schedule->call('App\Http\Controllers\MicroservicioController@verificarLibreDeudaDeTramites')->weekdays()->at('17:15');//Libre Deuda
+        $schedule->call('App\Http\Controllers\MicroservicioController@verificarBuiTramites')->weekdays()->at('17:20');//Bui
+        $schedule->call('App\Http\Controllers\MicroservicioController@completarBoletasEnTramitesAIniciar')->weekdays()->at('17:25');//Buscar Boleta Cenat
+        $schedule->call('App\Http\Controllers\MicroservicioController@emitirBoletasVirtualPago')->weekdays()->at('17:30');//Obtener Certificado Virtual Cenat
+        
+        //Revisar validaciones completas e iniciar en Sinalic
+	    $schedule->call('App\Http\Controllers\MicroservicioController@revisarValidaciones')->weekdays()->at('18:00');//Validaciones Completas
+        $schedule->call('App\Http\Controllers\MicroservicioController@enviarTramitesASinalic')->weekdays()->at('18:10');//Turnos a Enviar a Sinalic
         //FIN PRECHECK
     }
 
