17e4aee65315e4785640154cebb3d5792d575629
sistemasdghct <sistemasdghct@gmail.com>
2022-08-10 12:56:12 -0300

    adelanto API app movil

--- app/Http/Controllers/AppMovilController.php
+++ app/Http/Controllers/AppMovilController.php
@@ -37,21 +37,21 @@ class AppMovilController extends Controller
 
 	public function buscarTramite(Request $request)
     {
+		$codigopais = SysMultivalue::where('description','ILIKE',"%".$request->description."%")
+		->select('id')
+		->first();
+
+		$codigoelegido = $codigopais->id;
+
 	$tramite = Tramites::where('tramites.nro_doc',$request->nro_doc)
 	->select('tramites.tramite_id', 'tramites.nro_doc', 'tramites.sexo' , 'datos_personales.nombre' ,'datos_personales.apellido','tramites.pais')
 	->where('tramites.tipo_doc',$request->tipo_doc)
 	->where('tramites.sexo',$request->sexo)
-	->where('tramites.pais',$request->pais)
-	->where('estado',9)
+	->where('tramites.pais',$codigoelegido)
+	->where('estado',93)
 	->join('datos_personales','tramites.nro_doc','datos_personales.nro_doc')->orderBy('tramite_id','desc')->first();
 		//dd($tramite);
 
 		return response()->json($tramite);
     }
-
-
-	public function getCodigoPais(Request $request){
-		$paises = SysMultivalue::where('type','PAIS')->orderBy('description','ASC')->pluck('description','id');
-		return response()->json($paises);
-	}
 }
